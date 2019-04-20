f357f463ff176c17b196a314ce089c815b3d1ac6
Aaron Tree <jiujiubad@gmail.com>
2019-04-20 15:36:43 +0800

    1-1 to 1-10 尝试五种 jquery 效果

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
+  get '/jquery-1', to: 'pages#jquery_1'
+  get '/jquery-2', to: 'pages#jquery_2'
+  get '/jquery-3', to: 'pages#jquery_3'
+  get '/jquery-4', to: 'pages#jquery_4'
+  get '/jquery-5', to: 'pages#jquery_5'
 end
